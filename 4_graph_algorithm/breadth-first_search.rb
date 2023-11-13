d6c727b6b787ec056c6f9059cfdc6d2b2ad486ec
naopon-hamumu <115980030+naopon-hamumu@users.noreply.github.com>
2023-11-13 10:51:32 +0900

    幅優先探索

---
+++ 4_graph_algorithm/breadth-first_search.rb
@@ -0,0 +1,34 @@
+# 幅優先探索
+# 始点（最初にいる頂点）に近い頂点から、ゴール（指定された頂点）を探索する
+
+def breadth_first_search(graph, start_node)
+  queue = [start_node]
+  visited = { start_node => true }
+
+  while queue.any?
+    current = queue.shift
+    puts "訪問：#{current}"
+    graph[current].each do |neighbor|
+      next if visited[neighbor]
+      queue << neighbor
+      visited[neighbor] = true
+    end
+  end
+end
+
+# グラフの例
+graph = {
+  'A' => ['B', 'C'],
+  'B' => ['A', 'D', 'E'],
+  'C' => ['A', 'F'],
+  'D' => ['B'],
+  'E' => ['B', 'F'],
+  'F' => ['C', 'E']
+}
+
+# 幅優先探索の実行
+breadth_first_search(graph, 'A')
+
+[![Image from Gyazo]
+(https://i.gyazo.com/51f53be851de855e414974ea7fd997fd.png)]
+(https://gyazo.com/51f53be851de855e414974ea7fd997fd)
