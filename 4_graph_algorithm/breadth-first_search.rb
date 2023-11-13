210ccf003376ac34594e2677dd10b9bd73919eab
naopon-hamumu <115980030+naopon-hamumu@users.noreply.github.com>
2023-11-13 11:33:21 +0900

    幅優先探索

--- 4_graph_algorithm/breadth-first_search.rb
+++ 4_graph_algorithm/breadth-first_search.rb
@@ -1,22 +1,23 @@
 # 幅優先探索
 # 始点（最初にいる頂点）に近い頂点から、ゴール（指定された頂点）を探索する
 
-def breadth_first_search(graph, start_node)
-  queue = [start_node]
-  visited = { start_node => true }
+def breadth_first_search(graph, start)
+  queue = [start]
+  visited = []
 
-  while queue.any?
-    current = queue.shift
-    puts "訪問：#{current}"
-    graph[current].each do |neighbor|
-      next if visited[neighbor]
-      queue << neighbor
-      visited[neighbor] = true
-    end
+  until queue.empty?
+    node = queue.shift
+    next if visited.include?(node)
+
+    visited << node
+    neighbors = graph[node] - visited
+    queue += neighbors
   end
+
+  visited
 end
 
-# グラフの例
+# グラフの定義
 graph = {
   'A' => ['B', 'C'],
   'B' => ['A', 'D', 'E'],
@@ -27,4 +28,6 @@ graph = {
 }
 
 # 幅優先探索の実行
-breadth_first_search(graph, 'A')
+start_node = 'A'
+result = breadth_first_search(graph, start_node)
+puts "BFS starting from #{start_node}: #{result.join(', ')}"
